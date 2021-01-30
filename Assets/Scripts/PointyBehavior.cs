12feee455ae48e257279560b9ee97454662e22e4
Micah Cowan <micah@addictivecode.org>
2021-01-29 21:44:22 -0800

    Clamp the distance

--- Assets/Scripts/PointyBehavior.cs
+++ Assets/Scripts/PointyBehavior.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PointyBehavior : MonoBehaviour
 {
+    public float maximumDistance = 5;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,14 +17,25 @@ public class PointyBehavior : MonoBehaviour
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         Vector3 point = new Vector3(0, 0, 0);
-        // We want to find the point where the ray hits y = 0;
+        // We want to find the point where the ray hits y = 0 (or not 0, but same y as sheep);
         float runOverRiseX = ray.direction.y / ray.direction.x;
         float runOverRiseZ = ray.direction.y / ray.direction.z;
-        float sheepY = GameObject.Find("Sheep").transform.position.y;
-        float rise = ray.origin.y - sheepY;
+        Vector3 sheepPos = GameObject.Find("Sheep").transform.position;
+        float rise = ray.origin.y - sheepPos.y;
         point.x = ray.origin.x - rise / runOverRiseX;
         point.z = ray.origin.z - rise / runOverRiseZ;
-        point.y = sheepY;
+        point.y = sheepPos.y;
+
+        // Okay, we have it! But, we don't want to let the user drag _too_ far away from the sheep.
+        // Find the distance between our point and the sheep, and clamp it so we're never too far.
+        Vector3 diff = point - sheepPos;
+        float magnitude = diff.magnitude;
+        if (magnitude > maximumDistance)
+        {
+            diff *= maximumDistance / magnitude;
+            point = sheepPos + diff;
+        }
+
         transform.position = point;
     }
 }
