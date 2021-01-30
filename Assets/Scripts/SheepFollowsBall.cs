19d95b0223c7989a7de318b56f03bb2b8b77f4e9
HodgepodgeGarageGames <hodgepodgegaragegames@gmail.com>
2021-01-30 09:17:36 -0800

    Sheep Physics Stable

--- Assets/Scripts/SheepFollowsBall.cs
+++ Assets/Scripts/SheepFollowsBall.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class SheepFollowsBall : MonoBehaviour
 {
     public Vector3 offset = Vector3.zero;
-    public float power = 100.0f;
+    public float hpower = 100.0f;
+    public float vpower = 10.0f;
     [SerializeField] private Rigidbody ball = null;
     private Rigidbody rb = null;
 
@@ -20,7 +21,10 @@ public class SheepFollowsBall : MonoBehaviour
     {
         //rb.MovePosition(ball.position + (Vector3.up * vertical_offset));
 
-        rb.velocity = new Vector3((ball.position.x - (rb.position.x - offset.x)) * power, rb.velocity.y, (ball.position.z - (rb.position.z - offset.z)) * power);
+        rb.velocity = new Vector3(
+            ((ball.position.x + offset.x) - rb.position.x) * hpower,
+            (rb.position.y < ball.position.y + offset.y) ? ((ball.position.y + offset.y) - rb.position.y) * vpower : rb.velocity.y,
+            ((ball.position.z + offset.z) - rb.position.z) * hpower);
         rb.angularVelocity = ball.angularVelocity;
     }
 }
