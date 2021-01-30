a8bbae1352855c9b6d0d91fc2e6d5f91996d6882
Micah Cowan <micah@addictivecode.org>
2021-01-29 19:21:38 -0800

    Current silly state of things

---
+++ Assets/Scripts/PointyBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointyBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        point = Camera.main.transform.TransformVector(Input.mousePosition);
+        point.z = point.y;
+        point.y = 0;
+        transform.position = point;
+    }
+}
