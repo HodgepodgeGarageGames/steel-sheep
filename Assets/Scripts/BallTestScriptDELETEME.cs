5316172a23b9500bd31a59c70db535e32a009c70
HodgepodgeGarageGames <hodgepodgegaragegames@gmail.com>
2021-01-30 01:58:18 -0800

    Incorporated Sheep Into Main Scene

---
+++ Assets/Scripts/BallTestScriptDELETEME.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTestScriptDELETEME : MonoBehaviour
+{
+    [SerializeField] private Camera cam = null;
+    private Rigidbody rb = null;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+            rb.AddForce(new Vector3(cam.transform.right.normalized.x, 0.0f, cam.transform.right.normalized.z) * 10.0f);
+    }
+}
---
+++ Assets/Scripts/SheepFollowsBall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepFollowsBall : MonoBehaviour
+{
+    public Vector3 offset = Vector3.zero;
+    public float power = 100.0f;
+    [SerializeField] private Rigidbody ball = null;
+    private Rigidbody rb = null;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rb.MovePosition(ball.position + (Vector3.up * vertical_offset));
+
+        rb.velocity = new Vector3((ball.position.x - (rb.position.x - offset.x)) * power, rb.velocity.y, (ball.position.z - (rb.position.z - offset.z)) * power);
+        rb.angularVelocity = ball.angularVelocity;
+    }
+}
