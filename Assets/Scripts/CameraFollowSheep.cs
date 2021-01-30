be05c78e5e492a173c1e04db24f420e2c5241eae
Micah Cowan <micah@addictivecode.org>
2021-01-30 01:18:52 -0800

    Camera follows sheep

---
+++ Assets/Scripts/CameraFollowSheep.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowSheep : MonoBehaviour
+{
+    [Tooltip("object to follow (horizontally only)")]
+    public Transform target;
+
+    private Vector3 difference; // the difference to maintain relative to target.
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (target != null)
+        {
+            difference = transform.position - target.position;
+            difference.y = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = transform.position;
+        newPos.x = target.position.x + difference.x;
+        newPos.z = target.position.z + difference.z;
+        transform.SetPositionAndRotation(newPos, transform.rotation);
+    }
+}
