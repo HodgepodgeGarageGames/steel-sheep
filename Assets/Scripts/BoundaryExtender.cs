b566651d41bbb0f9f8f7809b48b09f3acba28c0a
Micah Cowan <micah@addictivecode.org>
2021-01-30 02:29:40 -0800

    Prototype: auto-extend boundaries

---
+++ Assets/Scripts/BoundaryExtender.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryExtender : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] bounds = GameObject.FindGameObjectsWithTag("Boundary");
+        foreach (GameObject b in bounds)
+        {
+            BoxCollider collide = b.GetComponent<BoxCollider>();
+            if (collide != null)
+            {
+                Vector3 newSize = collide.size;
+                newSize.y = 200;
+                collide.size = newSize;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
