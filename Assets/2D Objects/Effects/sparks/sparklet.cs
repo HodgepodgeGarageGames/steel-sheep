35d6cff35f145e67bf344d0c2a4e3ebc33fc3a76
HodgepodgeGarageGames <hodgepodgegaragegames@gmail.com>
2021-01-30 14:32:42 -0800

    Magnet Sheep (Appearance)

---
+++ Assets/2D Objects/Effects/sparks/sparklet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sparklet : MonoBehaviour
+{
+    Coroutine co = null;
+
+    public void go(float radius, float lifetime, float scale, float shaky)
+    {
+        if (co == null)
+            co = StartCoroutine(GO(radius, lifetime, scale, shaky));
+    }
+
+    private IEnumerator GO(float radius, float lifetime, float scale, float shaky)
+    {
+        transform.localPosition = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized * radius;
+        transform.LookAt(transform.parent);
+        transform.localScale = new Vector3(scale, scale, 1.0f);
+
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, Random.Range(-180.0f, 180.0f));
+
+        for (float t = 0.0f; t < lifetime; t += Time.deltaTime)
+        {
+            //transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z + Random.Range(-shaky, shaky));
+
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/SheepEffects.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepEffects : MonoBehaviour
+{
+    public float electric_radius = 0.015f;
+    public float spark_lifetime = 0.3f;
+    public float spark_scale = 0.01f;
+    public float spark_shaky_degrees = 2.0f;
+    public float spark_frequency = 20.0f;
+
+    [SerializeField] private GameObject[] spark_prefabs = new GameObject[0];
+    [SerializeField] private SkinnedMeshRenderer sheep_body = null;
+
+    private Coroutine sparker = null;
+
+    public void TurnOnElectricity()
+    {
+        TurnOffElectricity();
+
+        sheep_body.material.SetFloat("_Metallic", 0.7f);
+        sheep_body.material.SetColor("_EmissionColor", new Color(0.1f, 0.1f, 0.1f));
+
+        sparker = StartCoroutine(SPARK());
+    }
+
+    public void TurnOffElectricity()
+    {
+        sheep_body.material.SetFloat("_Metallic", 0.1f);
+        sheep_body.material.SetColor("_EmissionColor", new Color(0.42f, 0.42f, 0.42f));
+
+        if (sparker != null)
+            StopCoroutine(sparker);
+
+        sparker = null;
+    }
+
+    private IEnumerator SPARK()
+    {
+        while (true)
+        {
+            sparklet spark = Instantiate(spark_prefabs[Random.Range(0, spark_prefabs.Length)], transform).GetComponent<sparklet>();
+
+            spark.go(electric_radius, spark_lifetime, spark_scale, spark_shaky_degrees);
+
+            yield return new WaitForSeconds(1/spark_frequency);
+        }
+    }
+}
