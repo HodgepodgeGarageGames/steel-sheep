476a923728321b015020e053d9d4483fe1945263
Micah Cowan <micah@addictivecode.org>
2021-01-30 16:28:23 -0800

    Initial coin prefab, with spin

---
+++ Assets/Scripts/CoinBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinBehavior : MonoBehaviour
+{
+    public Transform coinBody;
+    [Tooltip("rotation speed in degrees per second")]
+    public float rotationSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float rotationAmount = rotationSpeed * Time.fixedDeltaTime;
+        coinBody.Rotate(new Vector3(rotationAmount, 0, 0));
+    }
+}
