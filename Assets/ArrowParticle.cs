907f097148220f9660acabc2e5744ce5de69a92f
HodgepodgeGarageGames <hodgepodgegaragegames@gmail.com>
2021-01-31 09:46:21 -0800

    Added propulsor graphics

---
+++ Assets/ArrowParticle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowParticle : MonoBehaviour
+{
+    private Coroutine co = null;
+    private SpriteRenderer rend = null;
+
+    private void Awake()
+    {
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    public void go(Vector3 s)
+    {
+        if (co == null)
+            co = StartCoroutine(GO(s));
+    }
+
+    private IEnumerator GO(Vector3 s)
+    {
+        transform.localPosition = new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), 0.1f);
+        transform.localScale = new Vector3(1.0f / (s.y * 10.0f), 1.0f / (s.x * 10.0f), -0.1f);
+
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, -0.1f - t);
+
+            rend.color = new Color(rend.color.r, rend.color.g, rend.color.b, 1.0f - (t*t));
+
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/ArrowsEffect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowsEffect : MonoBehaviour
+{
+    [SerializeField] private GameObject arrow = null;
+
+    private Coroutine co = null;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (co == null)
+            co = StartCoroutine(GO());
+    }
+
+    // Update is called once per frame
+    private IEnumerator GO()
+    {
+        while (true)
+        {
+            Vector3 range = (transform.right * transform.lossyScale.x) + (transform.forward * transform.lossyScale.z);
+
+            ArrowParticle ap = Instantiate(arrow, transform).GetComponent<ArrowParticle>();
+
+            ap.go(transform.lossyScale);
+
+            yield return new WaitForSeconds(0.08f);
+        }
+    }
+}
