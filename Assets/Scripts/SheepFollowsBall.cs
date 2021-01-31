f7927285ce32ea793b841a8b80cc22ddce41dc6c
HodgepodgeGarageGames <hodgepodgegaragegames@gmail.com>
2021-01-30 19:22:38 -0800

    Fixed the "stuck sheep" bug

--- Assets/Scripts/SheepFollowsBall.cs
+++ Assets/Scripts/SheepFollowsBall.cs
@@ -8,6 +8,7 @@ public class SheepFollowsBall : MonoBehaviour
     public float hpower = 100.0f;
     public float vpower = 10.0f;
     [SerializeField] private Rigidbody ball = null;
+    [SerializeField] private Rigidbody[] all_bones = new Rigidbody[0];
     private Rigidbody rb = null;
 
     // Start is called before the first frame update
@@ -19,8 +20,22 @@ public class SheepFollowsBall : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //rb.MovePosition(ball.position + (Vector3.up * vertical_offset));
+        //Teleport to ball if too far away
+        Vector3 d = ball.position - rb.position;
 
+        if (d.magnitude > 2.0f)
+        {
+            rb.position += d;
+            rb.velocity = ball.velocity;
+
+            foreach (Rigidbody bone in all_bones)
+            {
+                bone.position += d;
+                rb.velocity = ball.velocity;
+            }
+        }
+
+        //Inch toward ball
         rb.velocity = new Vector3(
             ((ball.position.x + offset.x) - rb.position.x) * hpower,
             (rb.position.y < ball.position.y + offset.y) ? ((ball.position.y + offset.y) - rb.position.y) * vpower : rb.velocity.y,
