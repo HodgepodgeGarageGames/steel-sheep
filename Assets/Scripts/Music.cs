bc178b3bbe7d743b1c209704fa46d43e0f1c8d9f
Micah Cowan <micah@addictivecode.org>
2021-02-01 00:06:55 -0800

    Added music.
    Launched by the SheepCamera on scenes that have that, and also separately by an object in the Bumper scene.

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public GameObject prefab;
+    private static bool instantiated = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!instantiated)
+        {
+            instantiated = true;
+            Object instance = Instantiate(prefab);
+            Object.DontDestroyOnLoad(instance);
+        }
+    }
+}
