92b439d4f41b7089cf6e9c052feb6923e07c17e2
Micah Cowan <micah@addictivecode.org>
2021-01-31 18:39:12 -0800

    End screen and (aborted) title screen

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    public float waitTime = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("WaitThenEnd");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator WaitThenEnd()
+    {
+        yield return new WaitForSeconds(waitTime);
+        SheepUtils.NextLevel();
+    }
+}
